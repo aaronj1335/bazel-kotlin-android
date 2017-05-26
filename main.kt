49acec0b547abcd2884a0838ca004ec63050586f
Aaron Stacy <aaronstacy@google.com>
2017-05-26 14:11:54 -0500

    woohoo got it working!

--- main.kt
+++ main.kt
@@ -1,11 +1,20 @@
-package com.aaronstacy.test
+package com.aaronstacy.app
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : Activity() {
+  val message: TextView by lazy { findViewById(R.id.flerg) as TextView }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
+    message.text = getString(R.string.hi)
+
+    findViewById(android.R.id.content).setOnClickListener {
+      Log.d("MainActivity", "click!")
+    }
   }
 }
